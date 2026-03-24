0c3567c70d88e77412eb67d46ba8b5fa19f71fb6
Kristine Karstens <karstens@pik-potsdam.de>
2026-03-24 19:23:19 +0100

    fix issues with cellular and weights

--- R/calcBEYield.R
+++ R/calcBEYield.R
@@ -27,23 +27,32 @@ calcBEYield <- function(cellular = FALSE) {
 
   if (!cellular) {
     weight  <- calcOutput("Croparea", sectoral = "kcr", physical = TRUE,
-                          cellular = TRUE, cells = "lpjcell", aggregate = FALSE)
+                          cellular = TRUE, aggregate = FALSE)
     weight  <- setYears(dimSums(weight[, "y1995", ], dim = 3), "y2010")
     mapping <- toolGetMappingCoord2Country()
     mapping$coordiso <- paste(mapping$coords, mapping$iso, sep = ".")
-    out <- toolAggregate(out, rel = mapping, weight = weight + 10^-10,
-                         from = "coordiso", to = "iso", dim = 1)
+    weightBegr <- weight
+    weightBegr[is.na(out[, , "begr"])] <- 0
+    weightBetr <- weight
+    weightBetr[is.na(out[, , "betr"])] <- 0
+    weightExp <- mbind(setNames(weightBegr, "begr"), setNames(weightBetr, "betr"))
+    out[is.na(out)] <- 0
+    out <- toolAggregate(out, rel = mapping, weight = weight,
+                         from = "coordiso", to = "iso", dim = 1, zeroWeight = "setNA")
     out    <- toolCountryFill(out, fill = NA)
     weight <- toolAggregate(weight, rel = mapping, from = "coordiso", to = "iso", dim = 1)
     weight <- toolCountryFill(weight, fill = 0)
+    weightOut <- mbind(setNames(weight, "betr"), setNames(weight, "begr"))
+    weightOut[is.na(out)] <- 0
+    out[is.na(out)]       <- 0 
   } else {
     weight <- calcOutput("Croparea", sectoral = "kcr", physical = TRUE,
-                         cellular = TRUE, cells = "lpjcell", aggregate = FALSE)
+                         cellular = TRUE, aggregate = FALSE)
     weight <- setYears(dimSums(weight[, "y1995", ], dim = 3), "y2010")
   }
 
   return(list(x            = out,
-              weight       = weight + 10^-10,
+              weight       = weight,
               unit         = "t DM ha-1 yr-1",
               description  = "Bioenergy crop yields from Li et al. (2020)",
               isocountries = !cellular))
