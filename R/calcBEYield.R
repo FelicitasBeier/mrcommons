41fa6477ee22341155ddc298c27c75bc2d0ebff2
Kristine Karstens <karstens@pik-potsdam.de>
2026-03-25 18:23:32 +0100

    update data to use cellular to regional aggregation and also return weights

--- R/calcBEYield.R
+++ R/calcBEYield.R
@@ -4,56 +4,54 @@
 #' Miscanthus and Switchgrass per cell. betr takes the maximum yield across
 #' Eucalypt, Poplar, and Willow per cell.
 #'
-#' @param cellular if TRUE returns 0.5deg grid level, if FALSE aggregates to country ISO level
-#' @return List of magpie objects with bioenergy yields in t DM ha-1 yr-1
+#' @param returnWeights if TRUE returns crop-specific cellular cropland area weights
+#'   (begr/betr) instead of the yields
+#' @return List of magpie objects with results on cellular level, weight, unit and description.
 #' @author Kristine Karstens
 #' @seealso [readLi2020()]
 #' @examples
 #' \dontrun{
-#' calcOutput("BEYield")
-#' calcOutput("BEYield", cellular = TRUE, aggregate = FALSE)
+#' calcOutput("BEYield", aggregate = FALSE)
+#' calcOutput("BEYield", returnWeights = TRUE, aggregate = FALSE)
 #' }
-calcBEYield <- function(cellular = FALSE) {
+calcBEYield <- function(returnWeights = FALSE) {
 
   x <- readSource("Li2020", convert = FALSE)
 
-  # begr: maximum yield across herbaceous bioenergy crops
+  # begr: cell-wise maximum across herbaceous crops; na.rm = TRUE retains cells
+  # where only one of the two species has a valid yield
   begr <- setNames(pmax(x[, , "Miscanthus"], x[, , "Switchgrass"], na.rm = TRUE), "begr")
 
-  # betr: maximum yield across all woody bioenergy crops
+  # betr: cell-wise maximum across woody crops; same na.rm rationale
   betr <- setNames(pmax(x[, , "Eucalypt"], x[, , "Poplar"], x[, , "Willow"], na.rm = TRUE), "betr")
 
   out <- mbind(begr, betr)
 
-  if (!cellular) {
-    weight  <- calcOutput("Croparea", sectoral = "kcr", physical = TRUE,
-                          cellular = TRUE, aggregate = FALSE)
-    weight  <- setYears(dimSums(weight[, "y1995", ], dim = 3), "y2010")
-    mapping <- toolGetMappingCoord2Country()
-    mapping$coordiso <- paste(mapping$coords, mapping$iso, sep = ".")
-    weightBegr <- weight
-    weightBegr[is.na(out[, , "begr"])] <- 0
-    weightBetr <- weight
-    weightBetr[is.na(out[, , "betr"])] <- 0
-    weightExp <- mbind(setNames(weightBegr, "begr"), setNames(weightBetr, "betr"))
-    out[is.na(out)] <- 0
-    out <- toolAggregate(out, rel = mapping, weight = weight,
-                         from = "coordiso", to = "iso", dim = 1, zeroWeight = "setNA")
-    out    <- toolCountryFill(out, fill = NA)
-    weight <- toolAggregate(weight, rel = mapping, from = "coordiso", to = "iso", dim = 1)
-    weight <- toolCountryFill(weight, fill = 0)
-    weightOut <- mbind(setNames(weight, "betr"), setNames(weight, "begr"))
-    weightOut[is.na(out)] <- 0
-    out[is.na(out)]       <- 0 
-  } else {
-    weight <- calcOutput("Croparea", sectoral = "kcr", physical = TRUE,
-                         cellular = TRUE, aggregate = FALSE)
-    weight <- setYears(dimSums(weight[, "y1995", ], dim = 3), "y2010")
+  # Cropland area (summed over all crops, 1995 base year) used as aggregation weight.
+  weight <- calcOutput("Croparea", sectoral = "kcr", physical = TRUE,
+                       cellular = TRUE, aggregate = FALSE)
+  weight <- setYears(dimSums(weight[, "y1995", ], dim = 3), "y2010")
+
+  # Crop-specific weights: cells with NA yield are set to 0 so they are
+  # excluded from weighted aggregation and flagged via zeroWeight = "setNA".
+  weightBegr <- weight
+  weightBegr[is.na(out[, , "begr"])] <- 0
+  weightBetr <- weight
+  weightBetr[is.na(out[, , "betr"])] <- 0
+  weightExp <- mbind(setNames(weightBegr, "begr"), setNames(weightBetr, "betr"))
+
+  if (returnWeights) {
+    return(list(x            = weightExp,
+                weight       = NULL,
+                unit         = "Mha",
+                description  = paste("Cellular cropland area weights used for aggregation",
+                                     "of bioenergy crop yields from Li et al. (2020)"),
+                isocountries = FALSE))
   }
 
   return(list(x            = out,
-              weight       = weight,
+              weight       = weightExp,
               unit         = "t DM ha-1 yr-1",
               description  = "Bioenergy crop yields from Li et al. (2020)",
-              isocountries = !cellular))
+              isocountries = FALSE))
 }
