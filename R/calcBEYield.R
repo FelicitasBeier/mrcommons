7bda6682f42d8cd5137204b7df26bb901e77c544
Kristine Karstens <karstens@pik-potsdam.de>
2026-05-07 16:58:18 +0200

    update weights for BE yield aggregation (including irrigation info)

--- R/calcBEYield.R
+++ R/calcBEYield.R
@@ -16,7 +16,7 @@
 #' }
 calcBEYield <- function(returnWeights = FALSE) {
 
-  x <- readSource("Li2020", convert = FALSE)
+  x <- setYears(readSource("Li2020", convert = FALSE), NULL)
 
   # begr: cell-wise maximum across herbaceous crops; na.rm = TRUE retains cells
   # where only one of the two species has a valid yield
@@ -28,25 +28,33 @@ calcBEYield <- function(returnWeights = FALSE) {
   out <- mbind(begr, betr)
 
   # Cropland area (summed over all crops, 1995 base year) used as aggregation weight.
-  weight <- calcOutput("Croparea", sectoral = "kcr", physical = TRUE,
+  weight <- calcOutput("Croparea", sectoral = "kcr", physical = TRUE, irrigation = TRUE,
                        cellular = TRUE, aggregate = FALSE)
-  weight <- setYears(dimSums(weight[, "y1995", ], dim = 3), "y2010")
+  weightDetail <- setYears(dimSums(weight[, "y1995", ], dim = "kcr"), NULL)
+  weight       <- setYears(dimSums(weight[, "y1995", ], dim = 3), NULL)
 
   # Crop-specific weights: cells with NA yield are set to 0 so they are
   # excluded from weighted aggregation and flagged via zeroWeight = "setNA".
   weightBegr <- weight
   weightBegr[is.na(out[, , "begr"])] <- 0
+  weightDetailBegr <- weightDetail
+  weightDetailBegr[is.na(out[, , "begr"])] <- 0
   weightBetr <- weight
   weightBetr[is.na(out[, , "betr"])] <- 0
+  weightDetailBetr <- weightDetail
+  weightDetailBetr[is.na(out[, , "betr"])] <- 0
   weightExp <- mbind(setNames(weightBegr, "begr"), setNames(weightBetr, "betr"))
+  weightDetailExp <- mbind(add_dimension(weightDetailBegr, dim = 3.1, add = "kbe", nm = "begr"),
+			   add_dimension(weightDetailBetr, dim = 3.1, add = "kbe", nm = "betr"))
   out[is.na(out)] <- 0
 
   if (returnWeights) {
-    return(list(x            = weightExp,
+    return(list(x            = weightDetailExp,
                 weight       = NULL,
                 unit         = "Mha",
-                description  = paste("Cellular cropland area weights used for aggregation",
-                                     "of bioenergy crop yields from Li et al. (2020)"),
+                description  = paste("Cellular cropland area weights (y1995 cropland area,",
+                                     "timeless) used for aggregation of bioenergy crop",
+                                     "yields from Li et al. (2020)"),
                 isocountries = FALSE))
   }
 
