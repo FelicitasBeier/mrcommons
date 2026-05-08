fffb9d7c5ae2dc33b6d42922e3876e2f6667d38c
FelicitasBeier <beier@pik-potsdam.de>
2026-05-08 09:20:17 +0200

    bugfix in calcClimateClass call in calcWoodyDensity

--- R/calcWoodDensity.R
+++ R/calcWoodDensity.R
@@ -21,7 +21,7 @@ calcWoodDensity <- function() {
   ipccDensity <- c(A = 0.59, B = 0.52, C = 0.45, D = 0.45, E = 0.45)
 
   # Get cell-level Koeppen-Geiger climate class shares (magpiecell = "ISO.number" format)
-  climateClass <- calcOutput("ClimateClass", datasource = "koeppen", cells = "magpiecell", aggregate = FALSE)
+  climateClass <- calcOutput("ClimateClass", datasource = "koeppen", aggregate = FALSE)
 
   # Use first year only (shares are time-invariant)
   if (!is.null(getYears(climateClass))) {
